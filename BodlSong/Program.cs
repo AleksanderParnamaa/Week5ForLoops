b7238f89fdb3d683eeabb8fd54ce5a0b0c50576f
Opilane TTHK <opilane@tthk.ee>
2021-09-29 12:10:50 +0300

    first commit

---
+++ BodlSong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BodlSong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 99; i > 1; i--)
+
+            {
+                if (i != 1)
+                {
+                    Console.WriteLine($"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down and pass it around {i - 1} bottles of beer on the wall");
+
+                }
+
+                else
+                {
+                    Console.WriteLine($"{i} bottle of beer on the wall, {i}bottle of beer.\n take one down and pass it around no more bottles of beer");
+                }
+                }
+                Console.WriteLine("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+            }
+        }
+    }
+
+
+    
+
+
---
+++ CalculatingSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculatingSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 3; i++) 
+            {
+                Console.WriteLine("sisesta kolm numbrit.");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                sum = sum + userNumber;
+            }
+            Console.WriteLine($"Your result is {sum}");
+        }
+    }
+}
